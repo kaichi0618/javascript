e34c04eb1713df6c750e840acc9550b3031d0f06
kaichi0618 <kaichi.1998.0618@gmail.com>
2021-10-02 18:06:17 +0900

    [finish]javascript

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
-let orange = 100;
-let apple = 135;
 
-if(orange < apple){
-  alert('みかんの値段がりんごより安い');
-} else if(orange == apple){
-  alert('みかんとりんごが同じ値段');
-} else{
-  alert('みかんの値段がりんごより高い');
+let i;
+let num = 0;
+
+for(i = 1; i<11; i = i + 1){
+  num = num + i;
 }
+
+alert('1から10までを足した結果は' + num + "です。");
+
+let str1 = 55;
+let str2 = 11;
+
+alert(str1 * str2 +'です。');
